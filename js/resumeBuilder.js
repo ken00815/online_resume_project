6bef0dc2c70121d811ed964f14d13cc5c60d8166
Ken <KenandChristie@christidkensmbp.lan>
2018-02-01 11:29:49 +0800

    Clean up unnecessary

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -195,12 +195,6 @@ var project = {
     }
 };
 
-// $(document).click(function(loc) {
-//   var x = loc.pageX;
-//   var y = loc.pageY;
-//   logClicks(x,y);
-// });
-
 //Internationalize the full name
 $("#main").append(internationalizeButton)
 
